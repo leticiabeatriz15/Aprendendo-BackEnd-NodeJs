66b85883fc0fc6696330b231e9ab1bf7c55cfee5
Leticia Beatriz <leticiabeatriz1508@gmail.com>
2024-11-22 12:12:48 -0300

    Banco de dados da MongoDB conectado ao servidor

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
+import conectarAoBanco from './src/config/dbConfig.js';
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const posts = [
     {
@@ -33,26 +39,31 @@ const posts = [
     }
   ];
    
-
 const app = express();
 app.use(express.json());
-
 
 app.listen(3000, () => {
     console.log("Servidor escutando!");
 });
 
-function buscaPostPorID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    });
+async function getTodosPosts(){
+  const db = conexao.db("instabytes");
+  const colecao = db.collection("posts");
+  return colecao.find().toArray();
 };
 
-app.get("/posts", (req, res) => {
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts();
     res.status(200).json(posts);
 });
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscaPostPorID(req.params.id)
-    res.status(200).json(posts[index]);
-});
+// function buscaPostPorID(id){
+//   return posts.findIndex((post) => {
+//       return post.id === Number(id)
+//   });
+// };
+
+// app.get("/posts/:id", (req, res) => {
+//     const index = buscaPostPorID(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
